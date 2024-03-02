6d3b5be89184a921d9ef9b533d6f11c1e8415996
muratapk <muratapk@hotmail.com>
2024-03-02 11:16:06 +0300

    Businness katmanın oluşturulması

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public  interface IGenericService<T>
+    {
+        void TAdd(T Entity);
+        void TDelete(T Entity);  
+        void TUpdate(T Entity);
+        List<T> TGetAll();
+        T GetById(int id);
+
+    }
+}
---
+++ BusinessLayer/Concrete/HostelManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class HostelManager : IHostelService
+    {
+        public Hostel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(Hostel Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Hostel Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Hostel> TGetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Hostel Entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
